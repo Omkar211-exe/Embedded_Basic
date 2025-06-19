2988c758d6ecbd57f9c8c046ee21535c49eac141
Omkar211-exe <omkarkulkarni211@gmail.com>
2025-06-19 13:45:11 +0530

    Add files via upload

---
+++ Q_1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define RANGE 500
+int main()
+{
+    int array[RANGE]={1,2,3,4,5,7,8,9,10};
+    for (int i=0 ; i<RANGE ; i++)
+    {
+        if (array[i]+1 != array[i+1])
+        {
+            printf("%d is not present in the array.",array[i]+1);
+            break;
+        }
+    }
+    return 0;
+}
